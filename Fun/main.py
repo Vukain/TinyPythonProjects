4e0ad47071360c840377407fe15f387e38bdde79
Vukain <55598879+Vukain@users.noreply.github.com>
2020-09-12 21:15:06 +0200

    Update main.py

--- Fun/main.py
+++ Fun/main.py
@@ -14,3 +14,37 @@ def random_with_sum(number_of_values, asserted_sum):
 
 for i in range(10):
     print(next(random_with_sum(3, 100)))
+    
+# 3 split partition quicksort
+x = input().split()
+
+def find_pivot(lst, start, end):
+    if int(lst[start]) <= int(lst[(start + end) // 2]) <= int(lst[end]):
+        return int(lst[(start + end) // 2])
+    elif int(lst[(start + end) // 2]) <= int(lst[end]) <= int(lst[start]):
+        return int(lst[end])
+    return int(lst[start])
+    
+
+def partition(lst, start, end):
+    aaa = []
+    bbb = []
+    pivot = find_pivot(lst, start, end)
+    small_right = start
+    pivot_right = start
+    for i in range(start, end + 1):
+        x = int(lst[i])
+        if  x < pivot:
+            lst.insert(small_right, lst.pop(i))
+            small_right += 1
+            pivot_right += 1
+        elif x == pivot:
+            lst.insert(pivot_right, lst.pop(i))
+            pivot_right += 1
+    return small_right, pivot_right - 1
+    
+a, b = partition(x, 0, len(x) - 1)
+
+print(a, b)
+for el in x:
+    print(el, end=" ")
